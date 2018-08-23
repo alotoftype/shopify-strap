418d3affde1116e6631e31ef7732224cbb24201b
Nikolas Savvidis <nicos.savidis@gmail.com>
2018-08-23 04:29:46 +0200

    pass only changed images

--- build/images.js
+++ build/images.js
@@ -1,25 +1,17 @@
 /* CONFIG */
 import {
-	config
+    config
 } from './../package.json';
 
 export default (gulp, plugins, mode) => {
-	return () => {
-		gulp
-			.src(config.images.src)
-			.pipe(plugins.flatten())
-			.pipe(plugins.image(mode.development({
-				pngquant: false,
-				optipng: false,
-				zopflipng: false,
-				jpegRecompress: false,
-				mozjpeg: false,
-				guetzli: false,
-				gifsicle: true,
-				svgo: false,
-				concurrent: 10,
-				quiet: false // defaults to false
-			})))
-				.pipe(gulp.dest(config.images.dest));
-			};
-	}
+    return () => {
+        gulp
+            .src(config.images.src)
+            .pipe(plugins.flatten())
+            .pipe(plugins.changed(config.images.dest, {
+                hasChanged: plugins.changed.compareContents
+            }))
+            .pipe(plugins.image())
+            .pipe(gulp.dest(config.images.dest));
+    };
+}
