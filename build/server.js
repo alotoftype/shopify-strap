b7c00c1a2701d3a77eb9909a781b6fc499f2b15a
Nikolas Savvidis <nicos.savidis@gmail.com>
2018-08-23 04:28:42 +0200

    clean up

--- build/server.js
+++ build/server.js
@@ -1,56 +1,48 @@
 /* CONFIG */
 import config from './../sync.config.json';
 
-/* Modules */
-import _ from 'lodash'
-import inquirer from 'inquirer';
-import fs from 'fs';
-import path from 'path';
-
 export default BrowserSync => {
 
-	return () => {
+    return () => {
 
-		let proxy = {};
+        let proxy = {};
 
-		config.targets.forEach(target => {
-			if(target.target_name === 'development') {
-				proxy.domain = target.primary_domain;
-				proxy.theme = target.theme_id;
-			}
-		});
+        config.targets.forEach(target => {
+            if (target.target_name === 'development') {
+                proxy.domain = target.primary_domain;
+                proxy.theme = target.theme_id;
+            }
+        });
 
-		return  BrowserSync({
-			proxy: `${proxy.domain}/?preview_theme_id=${proxy.theme}`,
-			files: ['./theme/assets/**'],
-			serveStatic: ['./theme/assets'],
-			https: true,
-			notify: true,
-			open: false,
-			reloadOnRestart: true,
-			https: {
-				key: "./build/ssl/localhost.key",
-				cert: "./build/ssl/localhost.crt"
-			},
-			snippetOptions: {
-				rule: {
-					match: /<\/head>/i,
-					fn: (snippet, match) => {
-						return snippet + match;
-					}
-				}
-			},
-			rewriteRules: [{
-					match: /(<link href=)(.*?)\/\/(cdn.shopify.com)(.*?)(stylesheet.css)/,
-					fn: () => '<link href="stylesheet.css'
-				},
-				{
-					match: /(<script src=)(.*?)\/\/(cdn.shopify.com)(.*?)(bundle.js)/,
-					fn: () => '<script src="bundle.js'
-				}
-			]
-		})
-
-		return stream
-	}
+        return BrowserSync({
+            proxy: `${proxy.domain}/?preview_theme_id=${proxy.theme}`,
+            files: ['./theme/assets/**'],
+            serveStatic: ['./theme/assets'],
+            https: true,
+            notify: true,
+            open: false,
+            reloadOnRestart: true,
+            https: {
+                key: "./build/ssl/localhost.key",
+                cert: "./build/ssl/localhost.crt"
+            },
+            snippetOptions: {
+                rule: {
+                    match: /<\/head>/i,
+                    fn: (snippet, match) => {
+                        return snippet + match;
+                    }
+                }
+            },
+            rewriteRules: [{
+                    match: /(<link href=)(.*?)\/\/(cdn.shopify.com)(.*?)(stylesheet.css)/,
+                    fn: () => '<link href="stylesheet.css'
+                },
+                {
+                    match: /(<script src=)(.*?)\/\/(cdn.shopify.com)(.*?)(bundle.js)/,
+                    fn: () => '<script src="bundle.js'
+                }
+            ]
+        });
+    }
 }
