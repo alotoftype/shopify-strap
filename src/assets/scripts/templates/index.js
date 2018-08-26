766da87457133044d8d824d82b549289ae826952
serge-tj <serge@tommyjohn.com>
2018-08-25 21:48:36 -0400

    edits after conversation with shanil

--- src/assets/scripts/templates/index.js
+++ src/assets/scripts/templates/index.js
@@ -4,16 +4,5 @@
 
 export default () => {
     console.log('ℹ️ This is the index template!')
-    window.onscroll = () => myFunction();
-    let header = document.querySelector('.headerTop');
 
-    var sticky = header.offsetTop;
-
-     function myfunction() {
-        if(window.pageYOffset > sticky ){
-            header.classList.add('sticky');
-        }else {
-            header.classList.remove('sticky');
-        }
-    }
 };
